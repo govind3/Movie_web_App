cfb3edd85590b7f39e679e6a37cbd3359cf0557a
Govind Kumar <govind85050@gmail.com>
2020-11-27 11:35:36 +0530

    first commit

---
+++ src/components/styles/GlobalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 5;
+    padding: 5;
+    box-sizing: border-box;
+  }
+`;
